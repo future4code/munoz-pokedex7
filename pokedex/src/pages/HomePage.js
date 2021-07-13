42d3f3ed7fec17d46c54f418ccc458643a2eeb72
unknown <dandan_187@hotmail.com>
2021-07-13 18:26:21 -0300

    pokelist

--- pokedex/src/pages/HomePage.js
+++ pokedex/src/pages/HomePage.js
@@ -1,18 +1,55 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
 import { useHistory } from 'react-router-dom'
 import { goToPokedex, goToDetails, goToHomePage } from '../router/coordinator'
 import {Pagina, Header, Lista, Card} from './Style/Styled'
 import Pokebola from '../imagem/Pokebola.png'
-import bulbasaur from '../imagem/bulbasaur.png'
-import image3 from '../imagem/image3.png';
-import image4 from '../imagem/image4.png';
-import image5 from '../imagem/image5.png';
-import image6 from '../imagem/image6.png';
-import image7 from '../imagem/image7.png';
+// import bulbasaur from '../imagem/bulbasaur.png'
+// import image3 from '../imagem/image3.png';
+// import image4 from '../imagem/image4.png';
+// import image5 from '../imagem/image5.png';
+// import image6 from '../imagem/image6.png';
+// import image7 from '../imagem/image7.png';
 
   // <h1>Página inicial da lista de pokemons</h1>// 
 
-export const HomePage = () => {
+export const HomePage = (props) => {
+    const [pokeList, setPokeList] = useState({})
+
+    const getPokeList = () => {
+        const url = `https://pokeapi.co/api/v2/pokemon/?limit=20`
+        axios
+        .get(url)
+        .then(response => {
+            setPokeList({pokeList: response.data.results})
+        })
+        .catch(err => {
+            console.log("erro!")
+        })
+    }
+
+
+    useEffect(() => {
+        getPokeList(props.pokeList)
+    },[props.pokeList])
+
+    const [pokemon, setPokemon] = useState ([])
+    const [pokeName, setPokeName] = useState("")
+    
+
+    
+    // const catchThemAll = (PokeNome) => {
+    //   const Url = `https://pokeapi.co/api/v2/pokemon/${PokeNome}/` 
+    //   axios
+    //   .get(Url)
+    //   .then((response) => setPokemon(response.data))
+    //   .catch((err) => console.log("Erro"))
+    // }
+
+    // useEffect (() => {
+    //     catchThemAll(props.pokemon)
+    // }, [props.pokemon])
+
     const history = useHistory()
 
     const goToHome = () => {
@@ -39,12 +76,20 @@ export const HomePage = () => {
             <Lista>
                 <div>
                 <Card>
-                <img src ={bulbasaur}/>
+                <div>
+                {pokeList.sprites && pokeList.sprites.map( (pokemon) => {
+                  return (    
+                  <img key={pokemon.name} src={pokemon.sprites.front_default} alt={pokemon.name} />
+                  )
+                }
+                )}
+                
+                </div>
                 <h1>Bulbasaur</h1>
                 <button onClick={goToDetailsPage}>Detalhes do pokemon</button>
                 </Card>
                 </div>
-                <div>
+                {/* <div>
                 <Card>
                 <img src ={image4}/>
                 <h1>Bulbasaur</h1>
@@ -83,7 +128,7 @@ export const HomePage = () => {
                 <img src ={bulbasaur}/>
                 <h1>Bulbasaur</h1>
                 <button onClick={goToDetailsPage}>Detalhes do pokemon</button>
-                </Card>
+                </Card> */}
 
 
             </Lista>
